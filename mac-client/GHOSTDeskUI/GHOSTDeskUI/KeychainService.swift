3946542a93ceca5b54e50e56d5dd8cd3f1c7ca89
Shamil Galiev <98946822+Shamilier@users.noreply.github.com>
2025-11-25 14:26:14 +0300

    Add configurable keychain reset on launch

--- mac-client/GHOSTDeskUI/GHOSTDeskUI/KeychainService.swift
+++ mac-client/GHOSTDeskUI/GHOSTDeskUI/KeychainService.swift
@@ -1,5 +1,11 @@
 import Security
 
+enum KeychainDebugConfig {
+    /// Toggle to clean the keychain every time the app launches while debugging.
+    /// Set to `false` to stop wiping stored credentials.
+    static let resetOnLaunch: Bool = true
+}
+
 func resetKeychain() {
     let secItemClasses: [CFString] = [
         kSecClassGenericPassword,
@@ -8,7 +14,7 @@ func resetKeychain() {
         kSecClassKey,
         kSecClassIdentity
     ]
-    
+
     for itemClass in secItemClasses {
         let query: [String: Any] = [kSecClass as String: itemClass]
         SecItemDelete(query as CFDictionary)
